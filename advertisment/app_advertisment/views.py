a6d9c0d983a3670c80053c47f796fda6da6b6aab
IgloBruh <asusivan2008@gmail.com>
2023-08-06 20:31:00 +0500

    homework for lesson_5

--- advertisment/app_advertisment/views.py
+++ advertisment/app_advertisment/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Это мой первый сайт!!!')
-
-def pobeda(request):
-    return HttpResponse('Это мой s сайт!!!')
+    return render(request, 'index.html')
